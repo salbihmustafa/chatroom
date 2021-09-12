fe774f88e1ae2e80977be4a32ec52d269c2b315a
Salbih Mustafa <salbihdmustafa@gmail.com>
2021-09-12 14:51:42 -0500

    check if user is logged in using onAuthStateChanged from firebase

--- src/main.js
+++ src/main.js
@@ -4,4 +4,17 @@ import router from './router'
 
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+import { projectAuth } from './firebase/config';
+import { onAuthStateChanged } from 'firebase/auth';
+
+let app;
+
+onAuthStateChanged(projectAuth, () => {
+    //every time there is a authentication change this will fire
+    if(!app) {
+        //if app doesn't have a value then mount app
+        app = createApp(App).use(router).mount('#app')
+    }
+})
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
 import Chatroom from '../views/Chatroom.vue'
+import { projectAuth } from '../firebase/config'
+
+//auth guard
+const requireAuth = (to, from, next) => {
+  let user = projectAuth.currentUser; //get current user
+  console.log('Current user in auth guard: ', user);
+  if(!user){
+    //if user is null
+    next({ name: 'Welcome' }); //redirect to welcome page
+  }else{
+    next(); //this will carry on to go to the route it needs to
+  }
+}
 
 const routes = [
   {
@@ -11,7 +24,8 @@ const routes = [
   {
     path: '/chatroom',
     name: 'Chatroom',
-    component: Chatroom
+    component: Chatroom,
+    beforeEnter: requireAuth //before entering chatroom, run requireAuth function
   }
 ]
 
